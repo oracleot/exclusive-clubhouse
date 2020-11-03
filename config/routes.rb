43a2bf7adf3c327d586620032664ece103889cbf
Meron Ogbai <okbaymeron@gmail.com>
2020-11-03 14:18:28 +0300

    Add only to posts router

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'posts#index'
-  resources :posts
+  resources :posts, only: %i[new create index show]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
